8dc38ec6c4d137728db41355ca5d29d6febd5338
erstazi <erstazi@gmail.com>
2026-08-07 12:30:06 -0400

    Adding translations and a thud for the didgeridoo

---
+++ didgeridoo.lua
@@ -0,0 +1,65 @@
+-- didgeridoo.lua
+
+local S = core.get_translator(core.get_current_modname())
+local modpath = core.get_modpath(core.get_current_modname())
+
+local DIDGERIDOO_COOLDOWN = 1.5 -- seconds
+
+local last_use = {}
+
+local didgeridoo_sounds = {
+  "jc_party_didgeridoo1",
+  "jc_party_didgeridoo2",
+  "jc_party_didgeridoo3",
+}
+
+core.register_craft({
+  output = "jc_party:didgeridoo",
+  recipe = {
+    {"group:tree", "", ""},
+    {"group:tree", "", ""},
+    {"group:tree", "", ""},
+  }
+})
+
+core.register_tool("jc_party:didgeridoo", {
+  description = S("Didgeridoo"),
+  inventory_image = "jc_party_didgeridoo.png",
+  wield_image = "jc_party_didgeridoo.png",
+  stack_max = 1,
+  wield_scale = {x = 1.5, y = 6.0, z = 1},
+  on_use = function(itemstack, user)
+    if not user then
+      return itemstack
+    end
+    local name = user:get_player_name()
+    local now = core.get_us_time()
+    if last_use[name] and (now - last_use[name]) < (DIDGERIDOO_COOLDOWN * 1000000) then
+      return itemstack
+    end
+    last_use[name] = now
+    core.sound_play(
+      didgeridoo_sounds[math.random(#didgeridoo_sounds)],
+      {
+        object = user,
+        gain = 1.0,
+        max_hear_distance = 48,
+      }
+    )
+    return itemstack
+  end,
+  on_place = function(itemstack, user, pointed_thing)
+    if not user then
+      return itemstack
+    end
+
+    core.sound_play("jc_party_didgeridoo_thud", {
+      object = user,
+      gain = 1.0,
+      max_hear_distance = 8,
+    })
+
+    return itemstack
+  end,
+})
+
